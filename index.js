53a1567cd9c908eb8c424656b99ac91f6f2818a8
Justin Lohner <lohner4@gmail.com>
2020-03-24 13:09:13 -0600

    got the stringSmash function working

--- index.js
+++ index.js
@@ -215,8 +215,8 @@ function removeApple(strings) {
  *
  * [2] Invoking `stringSmash` with `['a', 'b', 'c' ]` will return `abc`.
 */
-function stringSmash(/* code here */) {
-  /* code here */
+function stringSmash(strings) {
+  return strings.reduce((accumulator, string) => accumulator + string);
 }
 
 // A local community center is holding a fund raising 5k fun run and has invited
