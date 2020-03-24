c2c078905f585960be9327ff31dfc65c9f5e334b
Justin Lohner <lohner4@gmail.com>
2020-03-24 14:14:40 -0600

    got the counterMakerWithLimit function working, all tests now pass

--- index.js
+++ index.js
@@ -352,8 +352,16 @@ function counter2() {
  * counter() // should return 0
  * etc
 */
-function counterMakerWithLimit(/* CODE HERE */) {
-  /* CODE HERE */
+function counterMakerWithLimit(maxValue) {
+  let count = -1;
+  return function counter() {
+	count++;
+	if (count > maxValue) {
+	  count = 0;
+	  return count;
+	}
+	return count;
+  }
 }
 
 /////////////// END OF CHALLENGE ///////////////
